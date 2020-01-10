46836b7794ea6e5c8acb459c33763273a7bbc843
Wojtek Nowak <billnewmann@gmail.com>
2020-01-10 22:45:36 +0000

    Add player move prompt and code to display player move

--- js/script.js
+++ js/script.js
@@ -2,9 +2,25 @@ let computerMove = Math.floor(Math.random() * 3 + 1);
 
 
 if(computerMove == 1){
-  printMessage('Zagrałem kamień. Twój ruch!');
+  printMessage('Zagrałem kamień.');
 } else if (computerMove == 2) {
-	printMessage('Zagrałem papier. Twój ruch!');
+	printMessage('Zagrałem papier.');
 } else {
-	printMessage('Zagrałem nożyce. Twój ruch!');
+	printMessage('Zagrałem nożyce.');
+} //w sumie to możnaby to napisac tak jak ponizej.... warunek nadaje wartosc zmeinnej, a wartosc drukowana jest tylko przez ostatnia linijke kodu...
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+  playerMove = 'kamień';
+} else if (playerInput == 2) {
+	playerMove = 'papier';
+} else if (playerInput == 3) {
+	playerMove = 'nożyce';
 }
+
+printMessage('Twój ruch to: ' + playerMove);
