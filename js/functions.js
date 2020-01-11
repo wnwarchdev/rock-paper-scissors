6acabef154d3e06abc56b454d9cbab456fd96c8d
Wojtek Nowak <billnewmann@gmail.com>
2020-01-11 15:28:04 +0000

    Modified functions and script files

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,49 @@
-function printMessage(msg){
+function printMessage(msg){ //dekalaracja
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages(){ //dekalaracja
 	document.getElementById('messages').innerHTML = '';
+}
+
+function getMoveName(argMoveId){ //dekalaracja
+  if(argMoveId == 1){
+    return 'kamień';
+  }
+	else if(argMoveId == 2) {
+  	 return 'papier';
+  }
+	else if(argMoveId == 3) {
+		 return 'nożyce';
+	}
+	else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+}
+
+function displayResult(argComputerMove, argPlayerMove){ //dekalaracja
+
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove); //informuje o wykonanych ruchach
+
+  if( argComputerMove == 'kamień' && argPlayerMove == 'papier') { //trzy warunki wygranej, fajnie gdyby uzyc operatora OR i zapisac w jednej linii...
+    return('Ty wygrywasz!'); //zmieniłem na return z print zeby pokazac wynik w konsoli
+  }
+	else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+    return('Ty wygrywasz!');
+  }
+	else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+		return('Ty wygrywasz!');
+	}
+	else if (argComputerMove == argPlayerMove){ //warunek remisu
+    return('Remis...');
+  }
+	else if(argPlayerMove == 'nieznany ruch') { //w przypadku nieznanego ruchu
+		return('Spróbuj grać wedle reguł');
+	}
+	else {
+    return('Tym razem przegrywasz :('); //pozostałe warunki - przegrana
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,48 +1,21 @@
-let computerMove = Math.floor(Math.random() * 3 + 1); //losuje liczbę od 1 do 3
+let randomNumber = Math.floor(Math.random() * 3 + 1); //losuje liczbę od 1 do 3
+
+let computerMove = getMoveName(randomNumber);  //nadaje nazwę ruchu komputera wg klucza do zmiennej
 
 console.log('Psst, komputer wylosował: ' + computerMove + '... Masz szansę zdecydować o wyniku!');  //wyswietla wynik w konsoli
-
-if(computerMove == 1){ //wyswietla tekst w zaleznosci od wylosowanej liczby
-  printMessage('Zagrałem kamień.');
-} else if (computerMove == 2) {
-	printMessage('Zagrałem papier.');
-} else {
-	printMessage('Zagrałem nożyce.');
-} //w sumie to możnaby to napisac tak jak ponizej.... warunek nadaje wartosc zmeinnej, a wartosc drukowana jest tylko przez ostatnia linijke kodu...
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');  //pyta gracza o wybór zagrania
 
 console.log('Gracz wpisał: ' + playerInput);  //wyswietla wynik w konsoli
 
-let playerMove = 'nieznany ruch';  //nowa zmienna, o defaultowej wartosci
+let playerMove = getMoveName(playerInput); //nadaje nazwę ruchu gracza wg klucza do zmiennej
 
-if(playerInput == 1){  //przypisuje opis ruchu wg wybrajen wartości; jezeli nie zostanie wybrana jakakolwiek ze wskazanych w komunikacie, to zmienna pozostanie niezmieniona
-  playerMove = 'kamień';
-} else if (playerInput == 2) {
-	playerMove = 'papier';
-} else if (playerInput == 3) {
-	playerMove = 'nożyce';
-}
+let resultAnnounce = displayResult (computerMove, playerMove) //uruchamia funkcję z uzyciem dwu zmiennych
 
-printMessage('Twój ruch to: ' + playerMove); //wyswietla tekst z nazwą ruchu gracza
+printMessage (resultAnnounce)
 
-let gameResult = 'gra nierozstrzygnięta, spróbuj ponownie' //zmienna z defaultowa wartościa tekstowa, informująca o wyniku gry
+console.log('wyświetlana wiadomość to: ' + resultAnnounce);  //wyswietla wynik w konsoli
 
-if( computerMove == 1 && playerMove == 'papier'){ //trzy warunki wygranej gracza
-  gameResult = 'Ty wygrywasz! Gratulacje!';
-} else if (computerMove == 2 && playerMove == 'nożyce') {
-	gameResult = 'Doskonała intuicja, brawo!';
-} else if (computerMove == 3 && playerMove == 'kamień') {
-  gameResult = 'Jesteś Kasparovem te gry...';
-} else if (computerMove == 2 && playerMove == 'kamień') { //trzy warunki wygranej komputera
-  gameResult = 'Przegrałeś, poćwicz samemu i spróbuj jeszcze raz jak będziesz gotowy';
-} else if (computerMove == 3 && playerMove == 'papier') {
-  gameResult = 'Potrafię czytać w myślach, nie masz szans!';
-} else if (computerMove == 1 && playerMove == 'nożyce') {
-  gameResult = 'Jeszcze trochę kodu i będę władać tą smętną planetą! Koduj dalej!';
-} else if (computerMove == playerInput) { //chyba lepiej porównywać wartości liczbowe niż slowne... a najlepiej nie mieszac
-  gameResult = 'Remis!';
-}
-console.log('wyświetlana wiadomość to: ' + gameResult);  //wyswietla wynik w konsoli
 
-printMessage(gameResult); //wyswietla tekst z wynikiem
+
+//lub tak jezeli pominac konsolę: displayResult (computerMove, playerMove)
