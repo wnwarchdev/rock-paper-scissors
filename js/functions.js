9dce42264d0ef01d4ebec412625d31d206c86f87
Wojtek Nowak <billnewmann@gmail.com>
2020-01-12 20:51:35 +0000

    Add fadeIn() to functions.js for transition to counter; modified html and css accordingly

--- js/functions.js
+++ js/functions.js
@@ -18,6 +18,19 @@ function clearMessages(){ //dekalaracja
 	document.getElementById('messages').innerHTML = '';
 }
 
+function addFade(id){ //dekalaracja
+	"use strict";
+		var element = document.getElementById(id);
+		element.classList.remove("fadeIn");
+		console.log("un-faded!");
+		void element.offsetWidth;
+	  element.classList.add("fadeIn");
+		console.log("faded!");
+}
+
+
+
+
 function getMoveName(argMoveId){ //dekalaracja
   if(argMoveId == 1){
     return 'kamień';
@@ -40,21 +53,26 @@ function displayResult(argComputerMove, argPlayerMove){ //dekalaracja
 
   if( argComputerMove == 'kamień' && argPlayerMove == 'papier') { //trzy warunki wygranej, fajnie gdyby uzyc operatora OR i zapisac w jednej linii...
 		pointCount('counter-player');
+		addFade('counter-player');
 		return('Ty wygrywasz!'); //zmieniłem na return z print zeby pokazac wynik w konsoli
 
   }
 	else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
 		pointCount('counter-player');
+		addFade('counter-player');
 		return('Ty wygrywasz!');
 
   }
 	else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
 		pointCount('counter-player');
+		addFade('counter-player');
 		return('Ty wygrywasz!');
 
 	}
 	else if (argComputerMove == argPlayerMove){ //warunek remisu
-    return('Remis!');
+		addFade('counter-player');
+		addFade('counter-computer');
+		return('Remis!');
 
   }
 	/*else if(argPlayerMove == 'nieznany ruch') { //w przypadku nieznanego ruchu
@@ -62,6 +80,7 @@ function displayResult(argComputerMove, argPlayerMove){ //dekalaracja
 	}*/
 	else {
 		pointCount('counter-computer');
+		addFade('counter-computer');
     return('Tym razem przegrywasz...'); //pozostałe warunki - przegrana ; czy return konczy dzialanie funkcji? kiedy dalem point count za return to nie działalo...
   }
 }
